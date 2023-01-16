7b284785b657cae34fc7a350d133defb68cceda0
Lucas <lucasfigrock@gmail.com>
2023-01-16 19:37:28 -0300

    Primeira página concluída

---
+++ app.js
@@ -0,0 +1,14 @@
+button = document.querySelector("#button01");
+textButton = document.querySelector("#text-button");
+
+button.addEventListener("mouseover",()=>{
+    textButton.style.visibility = "visible";
+    button.style.backgroundColor = "#FFFF7D";
+    
+})
+
+button.addEventListener("mouseout",()=>{
+    textButton.style.visibility = "hidden";
+    button.style.backgroundColor="#3EB25E";
+})
+
