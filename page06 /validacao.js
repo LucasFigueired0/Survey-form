ea65e650ec2ab3184ac9b9cdbfaca438305f06c0
Lucas <lucasfigrock@gmail.com>
2023-01-23 12:35:18 -0300

    Validação da tela 6 concluída

--- page06 /validacao.js
+++ page06 /validacao.js
@@ -1,4 +1,6 @@
-const texto_area = document.querySelector("#campo-texto");
+const entrada_texto = document.querySelectorAll(".entradas-texto");
+
+console.log(entrada_texto)
 
 let input = document.querySelector(".button02");
 let botao_oculto = document.querySelector(".botao-escondido");
@@ -9,12 +11,31 @@ let cont = 0;
 
 
 input.addEventListener("click", (e) => {
+
+    cont = 0;
+
     e.preventDefault();
-    if(texto_area.value===""){
+
+    
+
+    for(let i in entrada_texto){
+        if(entrada_texto[i].value === ""){
+            cont++;
+        }
+    }
+
+    // if(entrada_texto[2])
+
+    console.log(cont);
+
+    if(cont > 0){
         alert("The text box cannot be empty!");
+    }
+    else if(entrada_texto[2].value > 120){
+        alert("Age cannot be higher than 120!");
     }
     else{
         link_enviar.click();
     }
-    console.log(texto_area.value);
+    
 })
